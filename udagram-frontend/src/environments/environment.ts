0a32fc900bb2476cc5a1418b6e2f41dcd52ed4e6
Kevin Wong <kjwong@Kevins-MBP.fios-router.home>
2024-08-31 04:16:23 -0400

    docker build not working after changing apihost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aed2c755b38fc4d63ab3c9ded03a8c08-1330546640.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
